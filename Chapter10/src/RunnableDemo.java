1c7706790c11b7ececb9819b6b8840d8ca7c3df8
junemp <junemp@gmail.com>
2018-09-06 07:34:06 +0900

    등록

---
+++ Chapter10/src/RunnableDemo.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class RunnableDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Runnable hellos = () -> {
+            for (int i = 1; i <= 1000; i++)
+                System.out.println("Hello " + i);
+        };
+
+        Runnable goodbyes = () -> {
+            for (int i = 1; i <= 1000; i++)
+                System.out.println("Goodbye " + i);
+        };
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        executor.execute(hellos);
+        executor.execute(goodbyes);
+
+        executor.shutdown();
+
+
+
+
+    }
+}
